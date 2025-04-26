456054bd21051ec8bff0c64e75ff82b4233e548e
huangshaoqi8888 <huangshaoqi8888@gmail.com>
2025-04-26 04:02:31 +0000

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 // middleware.ts
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
+export default clerkMiddleware({
   // Public routes that don't require authentication
   publicRoutes: ["/sign-in", "/sign-up"]
 });
